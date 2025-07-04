e051130e635a3b09b96f4fe461320f3dd8c727aa
pasqualevitiello <pasqualevitiello@gmail.com>
2025-07-04 19:22:29 +0200

    feat: add patterns

--- components/component-viewer.tsx
+++ components/component-viewer.tsx
@@ -78,8 +78,34 @@ export function ComponentViewer({ componentName, variantName }: ComponentViewerP
         defaultSize={panelSizes.mainPanel} 
         minSize={30}
       >
-        <div className={cn(`p-5 h-full overflow-y-auto ${componentData.viewerClassnames}`)}>
-          <Component {...props} />
+        <div className={cn(`relative p-5 h-full overflow-y-auto ${componentData.viewerClassnames}`)}>
+          {/* <svg fill="none" className="text-foreground/5 absolute inset-0 size-full">
+            <defs>
+              <pattern x="0" y="0" id="diagonal-lines" width="10" height="10" patternUnits="userSpaceOnUse">
+                <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" stroke="currentColor" />
+              </pattern>
+            </defs>
+              <rect fill="url(#diagonal-lines)" width="100%" height="100%" stroke="none" />
+          </svg> */}
+          <svg fill="none" className="text-foreground/10 absolute inset-0 size-full">
+            <defs>
+              <pattern x="0" y="0" id="texture" width="4" height="4" patternUnits="userSpaceOnUse">
+                <path d="M1 3h1v1H1V3zm2-2h1v1H3V1z" fill="currentColor" />
+              </pattern>
+            </defs>
+              <rect fill="url(#texture)" width="100%" height="100%" stroke="none" />
+          </svg>          
+          {/* <svg fill="none" className="text-foreground/8 absolute inset-0 size-full">
+            <defs>
+              <pattern x="0" y="0" id="tiny-checkers" width="8" height="8" patternUnits="userSpaceOnUse">
+                <path d="M0 0h4v4H0V0zm4 4h4v4H4V4z" fill="currentColor" />
+              </pattern>
+            </defs>
+              <rect fill="url(#tiny-checkers)" width="100%" height="100%" stroke="none" />
+          </svg>           */}
+          <div className="relative z-10">
+            <Component {...props} />
+          </div>
         </div>
       </ResizablePanel>
 
